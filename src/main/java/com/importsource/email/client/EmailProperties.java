5a47b4c9eeacb0265415bc24627b1bb7356ea670
importsource <hezhuofan24@126.com>
2016-09-22 16:40:26 +0800

    添加系统环境变量支持

--- src/main/java/com/importsource/email/client/EmailProperties.java
+++ src/main/java/com/importsource/email/client/EmailProperties.java
@@ -1,11 +1,13 @@
 package com.importsource.email.client;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
 
 import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import java.io.InputStream;   
-import java.io.IOException;   
+import org.apache.commons.logging.LogFactory;   
   
 /**  
 * 读取Properties文件
@@ -24,11 +26,11 @@ public final class EmailProperties {
         InputStream in = Thread.currentThread().getClass().getResourceAsStream("/importsource.email.properties");   
         try {   
             prop.load(in);   
-            from = prop.getProperty("xemail.from").trim();   
-            pwd = prop.getProperty("xemail.pwd").trim();
-            smtp = prop.getProperty("xemail.smtp").trim();
+            from = parse(prop.getProperty("xemail.from").trim());   
+            pwd = parse(prop.getProperty("xemail.pwd").trim());
+            smtp = parse(prop.getProperty("xemail.smtp").trim());
             if(prop.getProperty("xemail.protocol")!=null){
-            	protocol=prop.getProperty("xemail.protocol").trim();
+            	protocol=parse(prop.getProperty("xemail.protocol").trim());
             }
         } catch (IOException e) {   
             log.error(e);
@@ -41,7 +43,15 @@ public final class EmailProperties {
     private EmailProperties() {   
     }   
   
-    public static String getFrom() {   
+    private static String parse(String proVal) {
+		if(proVal.startsWith("@{") && proVal.endsWith("}")){
+			String envKey= proVal.substring("@{".length(), proVal.length()-1);
+			return System.getenv(envKey);
+		}
+		return proVal;
+	}
+
+	public static String getFrom() {   
         return from;   
     }   
   
@@ -63,7 +73,10 @@ public final class EmailProperties {
   
     public static void main(String args[]){   
         System.out.println(getFrom());   
-        System.out.println(getPwd());   
+        System.out.println(getPwd());  
+    
+    	
+    	
     }
 
 	
